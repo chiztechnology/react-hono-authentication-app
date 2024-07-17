5019e73a5bef57c99ece2a5f22fe239e709125e7
Israel CHIZUNGU <chizunguisrael@gmail.com>
2024-07-17 11:34:08 +0300

    configure Hono proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+// require('dotenv').config();
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // configure a proxy for hono
+  server : {
+    port : 8080,
+    proxy : {
+      "/api" : {
+        target : "https://7qk9m2xvu2.us-west-2.awsapprunner.com/v1",
+        changeOrigin : true,
+        rewrite : (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })

d0e12424729756f763da9a44e5602217eead2399
Israel CHIZUNGU <chizunguisrael@gmail.com>
2024-07-17 14:37:15 +0300

    Clear code

--- src/context/apiContext.ts
+++ src/context/apiContext.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const signIn_Auth = async (userData: any) => {
-    //  const userData = await c.req.json<{ username: string; password: string }>();
     let response = await axios.post(`https://7qk9m2xvu2.us-west-2.awsapprunner.com/v1/auth/login`, userData,
         {
             headers: {

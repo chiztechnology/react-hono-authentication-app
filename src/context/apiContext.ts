4be0dae6106996a349ea32eb9ae33169752f9472
Israel CHIZUNGU <chizunguisrael@gmail.com>
2024-07-17 14:36:54 +0300

    Fix deployment issue

--- src/context/apiContext.ts
+++ src/context/apiContext.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const signIn_Auth = async (userData: any) => {
     //  const userData = await c.req.json<{ username: string; password: string }>();
-    let response = await axios.post(`/auth/login`, userData,
+    let response = await axios.post(`https://7qk9m2xvu2.us-west-2.awsapprunner.com/v1/auth/login`, userData,
         {
             headers: {
                 'Content-Type': 'application/json',
